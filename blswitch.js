4755cca9f281f6230d4e6502009e39f74ef8525b
Peter <pggithub@gothager.se>
2015-11-14 13:37:36 +0100

    Initial version

--- blswitch.js
+++ blswitch.js
@@ -1,128 +1,204 @@
 /*jslint node: true */
 module.exports = function (RED) {
     "use strict";
-    var blcommon = require('./lib/blcommon'),
-        connectionPool = require('./lib/mqttConnectionPool'),
-        mongo = require('mongodb'),
-        MongoClient = mongo.MongoClient;
+    var blcommon = require('./lib/blcommon');
 
     /*
-        Defines the output node for a rule. Copied to a large extent from 66-mongodb.js
+        Defines the switch node.
     */
     function SwitchOutNode(n) {
         RED.nodes.createNode(this, n);
-        this.bldb = n.bldb;
-        this.dbConfig = RED.nodes.getNode(this.bldb);
-        this.blbroker = n.blbroker;
-        this.mqttConfig = RED.nodes.getNode(this.blbroker);
-        this.mqttPre = "/source/";
-        this.expire = n.expire;
+        this.times = n.times;
         this.name = n.name;
-        this.output = n.output;
 
         //timeout
-        if (n.timeoutUnits === "milliseconds") {
-            this.exptimeout = n.timeout;
-        } else if (n.timeoutUnits === "seconds") {
-            this.exptimeout = n.timeout * 1000;
-        } else if (n.timeoutUnits === "minutes") {
-            this.exptimeout = n.timeout * 1000 * 60;
-        } else if (n.timeoutUnits === "hours") {
-            this.exptimeout = n.timeout * 1000 * 60 * 60;
-        } else if (n.timeoutUnits === "days") {
-            this.exptimeout = n.timeout * 1000 * 60 * 60 * 24;
+        if (n.repeatUnits === "milliseconds") {
+            this.repeat = n.repeat;
+        } else if (n.repeatUnits === "seconds") {
+            this.repeat = n.repeat * 1000;
+        } else if (n.repeatUnits === "minutes") {
+            this.repeat = n.repeat * 1000 * 60;
+        } else if (n.repeatUnits === "hours") {
+            this.repeat = n.repeat * 1000 * 60 * 60;
+        } else if (n.repeatUnits === "days") {
+            this.repeat = n.repeat * 1000 * 60 * 60 * 24;
         }
-        this.expval = n.expval;
-        this.exptimer = null;
-        var node;
-        if (this.mqttConfig) {
-            node = this;
-            node.clientMqtt = connectionPool.get(node.mqttConfig.broker, node.mqttConfig.port, node.mqttConfig.clientid, node.mqttConfig.username, node.mqttConfig.password);
-            node.clientMqtt.on("connectionlost", function () {
-                node.error("mqtt disconnected");
-            });
-            node.clientMqtt.on("connect", function () {
-                node.log("mqtt connected");
-            });
-            node.clientMqtt.connect();
-        } else {
-            node.error("missing blbroker configuration");
-        }
+        this.timer = null;
+        var node = this;
+        //Set init status message
+        blcommon.setStatus(node, 0, "Not active");
 
-        if (this.dbConfig) {
-            node = this;
-            MongoClient.connect(this.dbConfig.url, function (err, db) {
-                node.clientDb = db;
-                if (err) {
-                    node.error(err);
-                    node.status({
-                        fill: "red",
-                        shape: "ring",
-                        text: "disconnected"
-                    });
-                } else {
-                    /* Initiate by retreiving what is in the db */
-                    blcommon.getKvp(db, "source", node.id, function (val, err) {
-                        if (!err) {
-                            blcommon.setStatus(node, 0, val);
-                        }
-                    });
-//console.log(node);
+        /* Holds messages from all different linkIds
+        Sturcture of incoming messages
+        { lid: xyz, status: 0/1, value:0-100, type: "rule"/"scenario"/"direct"}
+        */
+        node.allIds = [];
+        node.activeId = "none";
 
-                    /* When a message is received */
-                    node.on("input", function (msg) {
-                        /* Check if the value has an expiration set.
-                                Update the entry if not a new value has been received
-                        */
-                        if (node.expire) {
-                            //Clear the prev expiration
-                            if (node.exptimer) {
-                                clearTimeout(node.exptimer);
+
+        /* Method to create output message */
+        //Validate function of what the output msg should look like
+        node.getOutputMsg = function () {
+                var ids = node.allIds;
+                var id = node.id;
+                var sendit = false;
+
+                var omsg = {
+                    lid: id,
+                    status: 0,
+                    value: 0,
+                    type: "none"
+                };
+                for (var i = 0; i < ids.length; i++) {
+                    /* precense of type: rule < scenario < direct */
+                    if (ids[i].type === "direct") {
+                        /* If the input is active */
+                        if (ids[i].status === 1) {
+                            omsg.status = 1;
+
+                            /* Reset value if the type is changed */
+                            if (omsg.type !== "direct") {
+                                omsg.value = ids[i].value;
+                                node.activeId = ids[i].lid;
                             }
-                            //Set a new timer
-                            node.exptimer = setTimeout(function () {
-                                blcommon.setKvp(db, "source", node.id, node.expval, function (val, err) {
-                                    if (!err) {
-                                        blcommon.setStatus(node, -1, val);
-                                        blcommon.MqttPub(node.mqttConfig, node.clientMqtt, node.mqttPre + node.id, val);
-                                    }
-                                });
-                                //If output is enabled, send a new message with the expired value
-                                if (node.output) {
-                                    msg.payload = node.expval;
-                                    node.send(msg);
-                                }
-                            }, node.exptimeout);
-                        }
 
-                        blcommon.setKvp(db, "source", node.id, msg.payload, function (val, err) {
-                            if (!err) {
-                                blcommon.setStatus(node, 1, val);
-                                blcommon.MqttPub(node.mqttConfig, node.clientMqtt, node.mqttPre + node.id, val);
+                            //Set the active type
+                            omsg.type = ids[i].type;
 
+                            //Always use the highest value
+                            if (ids[i].value > omsg.value) {
+                                omsg.value = ids[i].value;
+                                /*Set the active id*/
+                                node.activeId = ids[i].lid;
                             }
-                        });
-
-                        //If there is an output send the message
-                        if (node.output) {
-                            node.send(msg);
                         }
 
-                    });
+                    } else if (ids[i].type === "scenario" && (omsg.type === "rule" || omsg.type === "none" || omsg.type === "scenario")) {
+                        /* If the input is active */
+                        if (ids[i].status === 1) {
+                            omsg.status = 1;
+
+                            /* Reset value if the type is changed */
+                            if (omsg.type === "rule") {
+                                omsg.value = ids[i].value;
+                                node.activeId = ids[i].lid;
+                            }
+
+                            //Set the active type
+                            omsg.type = ids[i].type;
+
+                            //Always use the highest value
+                            if (ids[i].value > omsg.value) {
+                                omsg.value = ids[i].value;
+                                /*Set the active id*/
+                                node.activeId = ids[i].lid;
+                            }
+                        }
+
+                    } else if (ids[i].type === "rule" && (omsg.type === "rule" || omsg.type === "none")) {
+                        /* If the input is active */
+                        if (ids[i].status === 1) {
+                            omsg.status = 1;
+
+                            //Set the active type
+                            omsg.type = ids[i].type;
+
+                            //Always use the highest value
+                            if (ids[i].value > omsg.value) {
+                                omsg.value = ids[i].value;
+                                /*Set the active id*/
+                                node.activeId = ids[i].lid;
+                            }
+                        }
+                    }
+                    if (omsg.type === "none") {
+                        node.activeId = "none";
+                    }
                 }
-            });
-        } else {
-            this.error("missing bldb configuration");
+                return omsg;
+            } //getOutputMsg
+
+        /* Send the message */
+        node.sendMsg = function (repeatCall) {
+            //Save prev id that is active
+            var prevActiveId = node.activeId;
+
+            if (node.allIds.length > 0) {
+                var output = node.getOutputMsg();
+                var msg = {
+                    payload: (output.state == 0 ? 0 : output.value)
+                };
+
+                /* Send the message the specified number of times */
+                if (prevActiveId != node.activeId || repeatCall) {
+                    /* Clear timer if it is not a repeatCall (called from timer) */
+                    if (!repeatCall && node.timer) {
+                        clearInterval(node.timer);
+                    }
+                    for (var i = 0; i < node.times; i++) {
+                        node.send(msg);
+                    }
+                    /* Set timer repeat function*/
+                    if (!repeatCall) {
+                        node.timer = setInterval(function () {
+                            node.sendMsg(true)
+                        }, node.repeat);
+
+                    }
+                }
+
+                //Set status message
+                var state = 1;
+                if (msg.payload === 0) {
+                    state = -1;
+                }
+                blcommon.setStatus(node, state, output.type + "/" + output.value + "%");
+            }
         }
 
-        this.on("close", function () {
-            if (this.clientDb) {
-                this.clientDb.close();
+
+        /* On received messages */
+        node.on("input", function (msg) {
+            //Validate inbound message
+            var errMsg = "";
+            if (msg.payload.lid == undefined) {
+                errMsg += "lid is missing in msg.payload\n";
             }
-            if (this.clientMqtt) {
-                this.clientMqtt.disconnect();
+            if (msg.payload.status == undefined) {
+                errMsg += "status is missing in msg.payload\n";
             }
+            if (msg.payload.value == undefined) {
+                errMsg += "value is missing in msg.payload\n";
+            }
+            if (msg.payload.type == undefined) {
+                errMsg += "type is missing in msg.payload\n";
+            }
+            if (errMsg !== "") {
+                console.log(errMsg);
+                return;
+            }
+            //Check if linkid already is in allIds array
+            var lid = null;
+            for (var i = 0; i < node.allIds.length; i++) {
+                if (msg.payload.lid === node.allIds[i].lid) {
+                    lid = i;
+                    break;
+                }
+            }
+
+            //Add or update entry in array
+            if (lid !== null) {
+                node.allIds[lid] = msg.payload;
+            } else {
+                node.allIds.push(msg.payload);
+            }
+
+            //Send messages
+            node.sendMsg();
         });
+
     }
+
+
     RED.nodes.registerType("bl-switch out", SwitchOutNode);
 };
