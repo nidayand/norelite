58a40d6727b05b66ae2820aebd4d7f2f337e2c3b
Peter <pggithub@gothager.se>
2015-11-14 21:01:14 +0100

    blvalue is to modify the switch on value where 0 = off and 100 is full

---
+++ blvalue.js
@@ -0,0 +1,28 @@
+/*jslint node: true */
+module.exports = function (RED) {
+    "use strict";
+    var blcommon = require('./lib/blcommon');
+
+
+    /*
+        Defines the output node for a rule. Copied to a large extent from 66-mongodb.js
+    */
+    function BelightValueOutNode(config) {
+        RED.nodes.createNode(this, config);
+        this.slider = config.slider;
+        var node = this;
+
+        /* When a message is received */
+        node.on("input", function(msg){
+            msg.payload.lid = node.id;
+            msg.payload.value = node.slider;
+            node.send(msg);
+        });
+
+        /* When a node is closed */
+        node.on("close", function(){
+            //Tidy up connections etc
+        });
+    }
+    RED.nodes.registerType("bl-value-out", BelightValueOutNode);
+};
