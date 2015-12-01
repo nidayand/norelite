86805d1ce965799a9a4ad89ca5e898328cf03d87
Peter <pggithub@gothager.se>
2015-12-01 15:06:55 +0100

    Added dim capabilities

--- device/nrl-tellstick.js
+++ device/nrl-tellstick.js
@@ -11,32 +11,42 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.code = config.code;
+        node.dimmable = config.dimmable;
 
         common.setStatus(node);
 
         /* When a message is received */
-        node.on("input", function(msg){
+        node.on("input", function (msg) {
             //Validate input
             var validate = common.validatePayload(msg.payload);
-            if (!validate.valid){
+            if (!validate.valid) {
                 node.warn(validate.error);
                 return;
             }
 
-            var nmsg = { device: node.code };
+            var nmsg = {
+                device: node.code
+            };
 
             var method;
             var dimlevel;
-            if (msg.payload.status === 1 && msg.payload.value === 100){
-                nmsg.method = "turnon";
-                common.setStatus(node, 1, "On");
-            } else if (msg.payload.status === 1 && msg.payload.value > 0){
-                nmsg.method = "dim";
-                nmsg.dimlevel = parseInt(255 * msg.payload.value/100);
-                common.setStatus(node, 1, "Dim "+msg.payload.value+"%");
+            if (node.dimmable) {
+                if (msg.payload.status === 1 && msg.payload.value > 0) {
+                    nmsg.method = "dim";
+                    nmsg.dimlevel = parseInt(255 * msg.payload.value / 100);
+                    common.setStatus(node, 1, "Dim " + msg.payload.value + "%");
+                } else {
+                    nmsg.method = "turnoff";
+                    common.setStatus(node, -1, "Off");
+                }
             } else {
-                nmsg.method = "turnoff";
-                common.setStatus(node, -1, "Off");
+                if (msg.payload.status === 1 && msg.payload.value > 0) {
+                    nmsg.method = "turnon";
+                    common.setStatus(node, 1, "On");
+                } else {
+                    nmsg.method = "turnoff";
+                    common.setStatus(node, -1, "Off");
+                }
             }
 
 
